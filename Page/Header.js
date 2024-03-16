3fe31887ef5a18da8bc880efc88dacfe8828fbf5
sonny faristrianto <sonnyf4r15@gmail.com>
2024-03-16 16:47:24 +0700

    tambah file app, folder page dan folder listdata

---
+++ Page/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Home | Product | Kontak | Tentang Kami</h1>
+            </div>
+        )
+    }
+}
+
+export default Header;
